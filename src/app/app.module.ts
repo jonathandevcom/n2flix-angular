6396fa5a294c9cdb2d586051d96552ce1a8cf35d
Jonathan FERNANDEZ <fernandez06130@gmail.com>
2022-06-23 10:51:25 +0100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
